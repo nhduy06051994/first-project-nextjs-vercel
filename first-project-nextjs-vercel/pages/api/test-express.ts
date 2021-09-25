d53d5265b58094c10c333f0e42c6b468f2af5f61
Nguyễn Hoàng Duy <duy06051994@gmail.com>
2021-09-25 20:25:39 +0700

    no message

---
+++ first-project-nextjs-vercel/pages/api/test-express.ts
@@ -0,0 +1,15 @@
+import { application } from "express";
+
+application.get("/api", (req, res) => {
+  const path = `/api/item/1}`;
+  res.setHeader("Content-Type", "text/html");
+  res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
+  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
+});
+
+application.get("/api/item/:slug", (req, res) => {
+  const { slug } = req.params;
+  res.end(`Item: ${slug}`);
+});
+
+module.exports = application;
